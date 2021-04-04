ced48283313c4a16a2398722de83073f4603e6fe
Ambar Bhatnagar <ambarbhatnagar@Ambars-MacBook-Pro.local>
2021-04-04 08:12:45 +0530

    First commit

--- src/index.js
+++ src/index.js
@@ -15,3 +15,9 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+window.MyWidget = function(idElement, param1, param2) {
+  alert();
+  let config = {param1, param2};
+  ReactDOM.render(<App config={config} />, document.getElementById(idElement));
+  return this;
+}
